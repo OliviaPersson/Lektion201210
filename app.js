4bf4ad40aabb5fd64b0a2d07c4b9b6f908365e6d
Olivia Persson <Olivia__P@hotmail.com>
2020-12-10 17:22:25 +0100

    html form for calculation

--- app.js
+++ app.js
@@ -40,21 +40,15 @@ http.createServer(function (req, res) {
         var expr = q.x + " " + operations[opIndex] + " " + q.y + " = " + txt;
         return res.end(expr.toString());
     }
+    else if (req.url == "/") {
+        fs.readFile('calculate.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        });
+    }
     else {
-        /* Register server: */
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write("<!DOCKTYPE html >\n");
-        res.write("     <html>\n");
-        res.write("         <head>\n");
-        res.write("             <title>" + req.url + "</title>\n")
-        res.write("         </head>\n");
-        res.write("         <body>\n");
-        res.write("             <h1>" + req.url + "</h1>\n")
-        res.write("             <p>" + "My first Paragraph" + "</p>\n")
-        res.write("         </body>\n");
-        res.write("     </html>");
-        //res.write(req.url);
-        console.log("Serving " + req.url);
+        res.write("Fel!");
         res.end();
     }
 }).listen(8080);
